5eb8fd9754fd24ee067f626e7c4493d6421ac593
rivervil16 <agustinriverovila@gmail.com>
2022-10-18 17:14:18 +0000

    hechas importaciones JWT y funcionando el endpoint para el login

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import json
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -15,4 +20,24 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
+    return jsonify(response_body), 200
+
+#PORQUE ESTE ES UN METODOS POST Y NO UN GET??
+@api.route('/login', methods=['POST'])
+def handle_login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    user_query = User.query.filter_by(email=email, password=password).first() #el metedo first es una buena practica para que el codigo devuelva al primero que cumpla con las 2 condiciones.and
+    if not user_query:
+        return jsonify({"mensaje":"usuario o contraseña no coinciden"}), 404
+    print(user_query.email) 
+#ahora creamos el acces token para ingreso
+    access_token = create_access_token(identity=user_query.email)
+    response_body = {
+        "msg":"Bienvenido al login",
+        "accessToken": access_token,
+        "email": user_query.email,
+        "id": user_query.id,
+    }
     return jsonify(response_body), 200
--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints.
+
+Imprtamos los from flask_jwt_extended import JWTManager
+from models import User. Es lo necesario para que funcione JWT.
 """
 import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
@@ -11,6 +14,9 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
+
+
 
 #from models import Person
 
@@ -18,6 +24,9 @@ ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
